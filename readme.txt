2b81afd8b64cfb7459bcaa19cbc7ab0f0d227b6b
dainryoo <dainryoo@gmail.com>
2017-02-11 19:03:08 -0500

    Initial Commit
    Bare bones: javascript functions work, but no styling.

---
+++ readme.txt
@@ -0,0 +1,52 @@
+Birthday Message Structures:
+1. Hope your birthday is as [acceptable] as you
+    >> OK, decent, alright, satisfactory, adequate, good, sufficient, passable, acceptable, tolerable
+2. I'm [aware] that it's your birthday
+    >> aware, told, under the impression, under the belief
+3. Happy Birthday, you're the best [pal]
+    >> dude, guy, gal, pal, bud, person, human, individual, human being
+4. Happy Birthday; hope next year is [acceptable]
+    >>
+5. Happy Birthday; hope next year isn't [uncomfortable]
+    >> uncomfortable, tired, sweaty, nervous, bad, ruffled, not good, ill, not great
+6. It's your birthday. How [acceptable]
+    >>
+
+
+Valentine's Message Structures:
+[Dear Valentine, Hey, Hey Valentine, Happy Valentine's Day, Hey you, Hey there, Hello]
+1. [Dear Valentine], you make my heart feel [uncomfortable]
+    >> 
+2. [Dear Valentine], you make me feel [acceptable]
+    >>
+3. [Dear Valentine], you're the best [pal]
+    >> 
+4. [Dear Valentine], I think you're very [acceptable].
+    >>
+5. [Dear Valentine], you're the most [acceptable] [pal].
+
+Apology Message Structures:
+[Sorry, Forgive me, Apologies, I apologize, Hello]
+1. [Sorry], I feel [uncomfortable] because of what I did
+    >> 
+2. [Sorry], I did a not-[acceptable] thing
+    >>
+3. [Sorry] for making you feel [uncomfortable]
+    >>
+4. [Sorry], I am a bad [pal]
+    >>
+5. [Sorry], I'm [aware] that I did a bad thing.
+
+
+Gratitude Message Structures:
+[Thanks, Thanks a lot, Thanks so much, Thank you, Much thanks, Many thanks, Hello] 
+1. [Thanks], you're [acceptable]
+    >> 
+2. [Thanks], I'm [aware] that you did the [acceptable] thing for me
+    >> 
+3. [Thanks], what you did was [acceptable]
+    >>
+4. [Thanks], you're the best [pal]
+    >>
+5. [Thanks], you're the most [acceptable] [pal].
+    >>
---
+++ script.js
@@ -0,0 +1,98 @@
+var acceptableSynonyms = ["acceptable", "adequate", "alright", "decent", "good", "ok", "passable", "satisfactory", "sufficient", "tolerable"];
+var awareSynonyms = ["aware", "told", "under the belief", "under the impression"];
+var palSynonyms = ["buddy", "dude", "gal", "guy", "human", "human being", "individual", "pal", "person"];
+var uncomfortableSynonyms = ["bad", "ill", "nervous", "not good", "not great", "ruffled", "sweaty", "tired", "uncomfortable"];
+
+var valentineFront = ["Dear Valentine", "Happy Valentine's Day", "Hello", "Hey", "Hey there", "Hey Valentine", "Hey you"];
+var sorryFront = ["Apologies", "Forgive me", "Hello", "I apologize", "Sorry"];
+var thanksFront = ["Hello", "Many thanks", "Much thanks", "Thanks", "Thanks a bunch", "Thanks a lot", "Thanks so much", "Thank you"];
+
+function pageSetup() {
+    document.getElementById("heartfeltMessage").style.visibility  = "hidden";
+}
+
+function birthdayMsg() {
+    document.getElementById("heartfeltMessage").style.visibility  = "visible";
+    var mainMsg = "";
+    var randNum = Math.floor((Math.random() * 6) + 1); //6 choices
+    if (randNum == 1) {
+        mainMsg = "Hope your birthday is as " + randWord(acceptableSynonyms) + " as you.";
+    } else if (randNum == 2) {
+        mainMsg = "I'm " + randWord(awareSynonyms) + " that it's your birthday.";
+    } else if (randNum == 3) {
+        mainMsg = "Happy Birthday, you're the best " + randWord(palSynonyms) + ".";
+    } else if (randNum == 4) {
+        mainMsg = "Happy Birthday, hope the next year is " + randWord(acceptableSynonyms) + ".";
+    } else if (randNum == 5) {
+        mainMsg = "Happy Birthday, hope the next year isn't " + randWord(uncomfortableSynonyms) + ".";
+    } else {
+        mainMsg = "It's your birthday. How " + randWord(acceptableSynonyms) + ".";
+    }
+    document.getElementById("heartfeltMessage").innerHTML = mainMsg;
+}
+
+function valentinesMsg() {
+    document.getElementById("heartfeltMessage").style.visibility  = "visible";
+    var frontMsg = randWord(valentineFront);
+    var mainMsg = "";
+    var randNum = Math.floor((Math.random() * 5) + 1); //5 choices
+    if (randNum == 1) {
+        mainMsg = frontMsg + ", you make my heart feel " 
+        + randWord(uncomfortableSynonyms) + ", but like in a good way.";
+    } else if (randNum == 2) {
+        mainMsg = frontMsg + ", you make me feel " + randWord(acceptableSynonyms) + "."
+    } else if (randNum == 3) {
+        mainMsg = frontMsg + ", you're the best " + randWord(palSynonyms) + ".";
+    } else if (randNum == 4) {
+        mainMsg = frontMsg + ", I think you're very " + randWord(acceptableSynonyms) + ".";
+    } else {
+        mainMsg = frontMsg + ", you're the most " + randWord(acceptableSynonyms) + " " + randWord(palSynonyms) + ".";
+    }
+    document.getElementById("heartfeltMessage").innerHTML = mainMsg;
+}
+
+function sorryMsg() {
+    document.getElementById("heartfeltMessage").style.visibility  = "visible";
+    var frontMsg = randWord(sorryFront);
+    var mainMsg = "";
+    var randNum = Math.floor((Math.random() * 5) + 1); //5 choices
+    if (randNum == 1) {
+        mainMsg = frontMsg + ",  I feel " + randWord(uncomfortableSynonyms)
+        + " because of what I did.";
+    } else if (randNum == 2) {
+        mainMsg = frontMsg + ", I did a not very " + randWord(acceptableSynonyms)
+        + " thing.";
+    } else if (randNum == 3) {
+        mainMsg = frontMsg + " for making you feel " + randWord(uncomfortableSynonyms);
+    } else if (randNum == 4) {
+        mainMsg = frontMsg + ", I am a bad " + randWord(palSynonyms);
+    } else {
+        mainMsg = frontMsg + ", I'm " + randWord(awareSynonyms)
+        + " that I did a bad thing"; 
+    }
+    document.getElementById("heartfeltMessage").innerHTML = mainMsg;
+}
+
+function thanksMsg() {
+    document.getElementById("heartfeltMessage").style.visibility  = "visible";
+    var frontMsg = randWord(thanksFront);
+    var mainMsg = "";
+    var randNum = Math.floor((Math.random() * 5) + 1); //5 choices
+    if (randNum == 1) {
+        mainMsg = frontMsg + ", you're " + randWord(acceptableSynonyms);
+    } else if (randNum == 2) {
+        mainMsg = frontMsg + ", I'm " + randWord(awareSynonyms) 
+        + " that you did that " + randWord(acceptableSynonyms) + " thing for me.";
+    } else if (randNum == 3) {
+        mainMsg = frontMsg + ", what you did was " + randWord(acceptableSynonyms);
+    } else if (randNum == 4) {
+        mainMsg = frontMsg + ", you're the best " + randWord(palSynonyms);
+    } else {
+        mainMsg = frontMsg + ", you're the most " + randWord(acceptableSynonyms) + randWord(palSynonyms) + ".";
+    }
+    document.getElementById("heartfeltMessage").innerHTML = mainMsg;
+}
+
+function randWord(wordArray) {
+    return wordArray[Math.floor(Math.random() * wordArray.length)];
+}
